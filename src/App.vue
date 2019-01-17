9a7025d79dd8ed75a92268210e48b269d6f4c836
Andrei Gavrila <Andrei.Gavrila@catalysts.cc>
2019-01-17 11:15:57 +0200

    Implement el gamal encrpytion

--- src/App.vue
+++ src/App.vue
@@ -1,108 +1,168 @@
 <template>
   <v-app>
     <v-content>
-      <v-container>
-        <app-description />
-        <v-snackbar v-model="showError" top color="error" :timeout="6000">
-          {{ errorMessage }}
-          <v-btn dark flat @click="showError = false">Close</v-btn>
-        </v-snackbar>
-        <v-layout row fill-height align-center justify-space-between>
-          <v-layout column justify-center>
-            <v-textarea class="plainText" :clearable="true" v-model="text" @keyup="encryptText"></v-textarea>
-            <v-btn color="primary" @click="encryptText">Encrypt</v-btn>
+      <v-container fill-height>
+        <v-layout column>
+          <v-layout row align-center justify-space-between>
+
+            <v-layout column>
+              <v-text-field v-model="p" label="Prime p" :rules="[validatePrime]"></v-text-field>
+              <v-btn @click="generateRandomPrime">Generate random</v-btn>
+            </v-layout>
+
+            <v-spacer></v-spacer>
+
+            <v-layout column>
+              <v-select :disabled="disableGenerators" label="Generator g" :items="generators" v-model="g"></v-select>
+              <v-btn :disabled="disableGenerators || !generators" @click="generateRandomGenerator">Select random</v-btn>
+            </v-layout>
+
+            <v-spacer></v-spacer>
+
+            <v-layout column>
+              <v-text-field :disabled="disablePrivateKey" label="Private key a" v-model="a" :rules="[validatePrivateKey]"></v-text-field>
+              <v-btn :disabled="disablePrivateKey" @click="generateRandomA">Generate random</v-btn>
+            </v-layout>
+
+            <v-spacer></v-spacer>
+
+            <v-layout column>
+              <v-text-field :disabled="disablePrivateKey" v-model='k' label="Private key k" :rules="[validatePrivateKey]"></v-text-field>
+              <v-btn :disabled="disablePrivateKey" @click="generateRandomK">Generate random</v-btn>
+            </v-layout>
           </v-layout>
-          <v-layout column justify-begin align-center>
-            <v-text-field placeholder="a, b" label="Encryption key" prefix="(" suffix=")" v-model="formattedEncryptionKey" @change="calculateDecryptionKey"></v-text-field>
-            <v-text-field placeholder="a, b" label="Decryption key" prefix="(" suffix=")" v-model="formattedDecryptionKey" @change="calculateEncryptionKey"></v-text-field>
+
+          <v-layout row align-center justify-space-between>
+            <v-spacer></v-spacer>
+            <v-layout column>
+              <v-text-field label="Public key" readonly :value="publicKey"></v-text-field>
+              <v-btn @click="allRandom">All random</v-btn>
+
+            </v-layout>
+            <v-spacer></v-spacer>
+
           </v-layout>
-          <v-layout column justify-center>
-            <v-textarea class="cipherText" :clearable="true" v-model="cipherText" @keyup="decryptText"></v-textarea>
-            <v-btn color="primary" @click="decryptText">Decrypt</v-btn>
+
+          <v-layout row align-center justify-space-between>
+            <v-layout column justify-center>
+              <v-textarea :disabled="!canEncrypt" placeholder="plain text" class="plainText" clearable v-model="plainText" @keyup="encryptText" :rules="[validText]"></v-textarea>
+              <v-btn :disabled="!canEncrypt" color="primary" @click="encryptText">Encrypt</v-btn>
+            </v-layout>
+            <v-spacer />
+            <v-layout column justify-center>
+              <v-textarea :disabled="!canDecrypt" placeholder="cipher text" class="cipherText" clearable v-model="cipherText" @keyup="decryptText" :rules="[validText]"></v-textarea>
+              <v-btn :disabled="!canDecrypt" color="primary" @click="decryptText">Decrypt</v-btn>
+            </v-layout>
           </v-layout>
         </v-layout>
-        <v-layout></v-layout>
+
       </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-  import cipher from "./affineCipher"
+  import elgamal from "./ElGamal"
   import AppDescription from "./AppDescription"
 
-  const keyRE = /^\s*(\d+),\s*(\d+)\s*$/
   export default {
     name: 'App',
     components: {AppDescription},
+    mounted() {
+      elgamal.populatePrimeNumbers()
+    },
     data() {
       return {
-        encryptionKey: {a: 7, b: 5},
-        decryptionKey: {a: 4, b: 7},
-        text: "",
-        cipherText: "",
-        formattedDecryptionKey: "7,5",
-        formattedEncryptionKey: "4,7",
-        showError: false,
-        errorMessage: "",
+        p: '',
+        g: '',
+        ga: '',
+        a: '',
+        k: '',
+        generators: [],
+
+        plainText: '',
+        cipherText: '',
       }
     },
     methods: {
-      hideError() {
-        this.showError = false
-        this.errorMessage = ""
+      generateRandomPrime() {
+        this.p = elgamal.getRandomPrime()
       },
-      error(message) {
-        this.errorMessage = message
-        this.showError = true
+      validatePrime() {
+        if (!this.validPrime) {
+          this.g = ''
+          this.generators = []
+          this.ga = ''
+          this.a = ''
+          this.k = ''
+          return "Must be a prime number"
+        }
+        if (this.p !== '') {
+          setTimeout(() => {
+            elgamal.getRoots(this.p).then((roots) => this.generators = roots)
+          }, 0)
+        }
+        return true
+      },
+      generateRandomGenerator() {
+        this.g = elgamal.generateRandomGenerator(this.generators)
+        this.ga = elgamal.fastPow(this.g, this.a, this.p)
+      },
+      generateRandomA() {
+        this.a = elgamal.generateRandomPrivateKey(this.p)
+        this.ga = elgamal.fastPow(this.g, this.a, this.p)
+      },
+      generateRandomK() {
+        this.k = elgamal.generateRandomPrivateKey(this.p)
+      },
+      validatePrivateKey(v) {
+        if (!elgamal.isValidPrivateKey(this.p, v)) {
+          return "Invalid private key"
+        }
+        return true;
       },
       encryptText() {
-        this.hideError()
-        this.text = this.text.toLowerCase()
-        let cipherText = cipher.encrypt(this.text, this.encryptionKey)
-        if (cipherText === undefined) {
-          return this.error('Some characters cannot be encrypted. Make sure you are using the lowercase english alphabet or spaces.')
-        }
-        this.cipherText = cipherText
+        this.cipherText = elgamal.isValidText(this.plainText) ? elgamal.encrypt(this.plainText, this.p, this.g, this.ga, this.k) : ''
+
       },
       decryptText() {
-        this.hideError()
-        this.cipherText = this.cipherText.toLowerCase()
-        let text = cipher.encrypt(this.cipherText, this.decryptionKey)
-        if (text === undefined) {
-          return this.error('Some characters cannot be decrypted. Make sure you are using the uppercase english alphabet or spaces.')
-        }
-        this.text = text
+        this.plainText = elgamal.isValidText(this.cipherText) ? elgamal.decrypt(this.cipherText, this.p, this.g, this.ga, this.a) : ''
       },
-      calculateDecryptionKey() {
-        this.hideError()
-        let match = this.formattedEncryptionKey.match(keyRE)
-        if (!match) {
-          return this.error('Invalid format. Please use a,b')
+      validText(text) {
+        if (!elgamal.isValidText(text)) {
+          return "Only letters and spaces please"
         }
-        this.encryptionKey = {a: parseInt(match[1]), b: parseInt(match[2])}
-        let decryptionKey = cipher.getDecryptionKey(this.encryptionKey)
-        if (!decryptionKey) {
-          return this.error(`${match[1]} has a common divisor with 27(the length of the alphabet used). Please try a different value for 'a'!`)
-        }
-        this.decryptionKey = decryptionKey
-        this.formattedDecryptionKey = this.decryptionKey.a + "," + this.decryptionKey.b
-        this.encryptText()
+        return true
       },
-      calculateEncryptionKey() {
-        this.hideError()
-        let match = this.formattedDecryptionKey.match(keyRE)
-        if (!match) {
-          return this.error('Invalid format. Please use a,b')
+      async allRandom() {
+        this.generateRandomPrime()
+        this.generators = await elgamal.getRoots(this.p)
+        this.generateRandomGenerator()
+        this.generateRandomA()
+        this.generateRandomK()
+      }
+    },
+    computed: {
+      publicKey() {
+        if (this.p === '' || this.g === '' || this.a === '') {
+          return "Not yet calculable"
         }
-        this.decryptionKey = {a: parseInt(match[1]), b: parseInt(match[2])}
-        let encryptionKey = cipher.getDecryptionKey(this.decryptionKey)
-        if (!encryptionKey) {
-          return this.error(`${match[1]} has a common divisor with 27(the length of the alphabet used). Please try a different value for 'a'!`)
-        }
-        this.encryptionKey = encryptionKey
-        this.formattedEncryptionKey = this.encryptionKey.a + "," + this.encryptionKey.b
-        this.decryptText()
+        return `(${this.p}, ${this.g}, ${this.ga})`
+      },
+      validPrime() {
+        return this.p === null || this.p === '' || elgamal.isPrime(this.p)
+      },
+      disableGenerators() {
+        return this.p === '' || !this.validPrime
+      },
+      disablePrivateKey() {
+        return this.p === '' || !this.validPrime
+      },
+      canEncrypt() {
+        return this.p !== '' && this.g !== '' && this.ga !== '' && this.k !== ''
+      },
+      canDecrypt() {
+        return this.p !== '' && this.g !== '' && this.ga !== '' && this.a !== ''
       },
     },
 
---
+++ src/ElGamal.js
@@ -0,0 +1,123 @@
+const math = require("mathjs")
+const ALPHABET = " abcdefghijklmnopqrstuvwxyz"
+const ENCRYPTED_LENGTH = 2;
+
+export default {
+  primeNumbers: [],
+
+  isPrime(x) {
+    return math.isPrime(x)
+  },
+
+  populatePrimeNumbers() {
+    const maxPrime = math.pow(ALPHABET.length, ENCRYPTED_LENGTH) - 1;
+    for (let x = 3; x <= maxPrime; x+=2) {
+      if (this.isPrime(x)) {
+        this.primeNumbers.push(x)
+      }
+    }
+  },
+
+  getRandomPrime() {
+    return this.primeNumbers[math.floor(math.random(this.primeNumbers.length))]
+  },
+
+  isPrimitiveRoot(g, p) {
+    let o = 1
+    let k = g % p;
+    while (k > 1) {
+      o++;
+      k = (k * g) % p;
+    }
+    return o === (p - 1);
+  },
+
+  fastPow(base, exp, mod) {
+    if (exp === 0) return 1;
+    if (exp % 2 === 0) {
+      return math.pow(this.fastPow(base, exp / 2, mod), 2) % mod;
+    }
+    return (base * this.fastPow(base, exp - 1, mod)) % mod;
+  },
+
+  async getRoots(p) {
+    const roots = [];
+
+    for (let r = 2; r < p; r += 1) {
+      if (this.isPrimitiveRoot(r, p)) {
+        roots.push(r);
+      }
+    }
+
+    return roots;
+  },
+
+  isValidText(text) {
+    const letterSet = new Set(ALPHABET.split(''))
+    for (let letter of text.toLowerCase()) {
+      if (!letterSet.has(letter)) {
+        return false;
+      }
+    }
+    return true;
+  },
+
+  encryptedToLetters(number) {
+    let letters = ""
+    while (number > 0) {
+      letters = ALPHABET[number % ALPHABET.length] + letters
+      number = Math.floor(number / 27)
+    }
+    return letters
+  },
+
+  lettersToEncrypted(letters) {
+    let pow = 1
+    let value = 0;
+    for (let letter of letters.toLowerCase().split('').reverse().join('')) {
+      value += ALPHABET.indexOf(letter) * pow
+      pow *= ALPHABET.length
+    }
+    return value
+  },
+
+  encrypt(text, p, g, ga, k) {
+    let encryptedText = ""
+    for (let letter of text.toLowerCase()) {
+      let alpha = this.fastPow(g, k, p)
+      let beta = math.mod(this.fastPow(ga, k, p) * ALPHABET.indexOf(letter), p)
+      encryptedText += this.encryptedToLetters(alpha) + this.encryptedToLetters(beta)
+    }
+    return encryptedText
+  },
+
+  decrypt(text, p, g, ga, a) {
+    while (text.length % (2 * ENCRYPTED_LENGTH) !== 0) {
+      text += " "
+    }
+
+    let decrypted = ""
+    for (let i = 0; i < text.length; i += 2 * ENCRYPTED_LENGTH) {
+      let alpha = this.lettersToEncrypted(text.slice(i, i + ENCRYPTED_LENGTH))
+      let beta = this.lettersToEncrypted(text.slice(i + ENCRYPTED_LENGTH, i + 2 * ENCRYPTED_LENGTH))
+
+      let m = math.mod(beta * this.fastPow(alpha, p - 1 - a, p), p)
+      decrypted += ALPHABET[m]
+    }
+    return decrypted
+  },
+
+  generateRandomPrivateKey: (p) => {
+    return math.floor(math.random(1, p - 1))
+  },
+
+  isValidPrivateKey: (p, k) => {
+    return k > 0 && k < p - 1;
+  },
+
+  generateRandomGenerator(generators) {
+    return generators[math.floor(math.random(generators.length))]
+  },
+
+
+}
